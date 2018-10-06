e6c89cc52b2c736598f343720639ab44465ad0f2
Stefan <Stefan.Murga@hotmail.com>
2018-10-06 10:38:29 -0400

    add logging to download script

--- DataModule/download_all.py
+++ DataModule/download_all.py
@@ -1,11 +1,21 @@
 import os
 import json
 import time
+import logging
 
+import definitions
 from DataModule.DataManager import DataManager
 
 
 def main():
+    logging.basicConfig(format='%(asctime)s - %(message)s',
+                        datefmt='%d-%b-%y %H:%M:%S',
+                        level=logging.INFO,
+                        filename=os.path.join(definitions.DATABASE_DIR, 'download_log'))
+
+    LOGGER = logging.getLogger()
+    LOGGER.info('Download all script starting up....')
+
     with open(os.path.join(os.path.dirname(__file__), 'symbols.json')) as infile:
         f = json.load(infile)
     indexes = f['indexes']
@@ -13,6 +23,7 @@ def main():
 
     dm = DataManager()
     for symbol in indexes + sp500:
+        LOGGER.info('Downloading %s' % symbol)
         dm.download(symbol)
         time.sleep(15)
 
